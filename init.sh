96810828e71532246511544a09d329660166a8ce
Serabass <serabass565@gmail.com>
2019-08-17 06:09:17 +0600

    upd

--- init.sh
+++ init.sh
@@ -29,10 +29,52 @@ mv ../.init/backend ./
 
 cd backend
 
+cat package.json | json -e "this.name = '@$PROJECTNAME/backend'" > package.json
+
 yarn
 
 yarn ts-jest config:init
 
+cd ..
+
+
+# Крафтим новое ангуляр-приложение (в ходе установки консоль задаст несколько вопросов)
+yarn ng new frontend
+cd ..
+
+# Не спрашивайте зачем это здесь и почему вот так.
+mv frontend packages
+
+# Проваливаемся в директорию нового приложения
+cd packages/frontend
+
+# Ставим необходимые модули
+yarn add @ngx-resource/core \
+         @ngx-resource/handler-ngx-http \
+         @uirouter/angular \
+         faker \
+         jquery \
+         lodash \
+         moment \
+         reflect-metadata
+
+# Этот ставим отдельно, потому что там происходит ангуляровская магия с развёртыванием
+# (в ходе установки консоль задаст несколько вопросов)
+yarn ng add ng-zorro-antd --i18n=ru_RU
+
+# Возвращаемся в корень
 cd ../..
+
+# Забираем конфиг для прокси (он нам нужен для удобной работы с бэкендом на этапе разработки)
+mv .init/frontend/proxy.config.json packages/frontend
+
+# Снова идём к фронту
+cd packages/frontend
+
+# Обновляем задачу start, чтобы работало через прокси
+cat package.json | json -e "this.scripts.start += ' --proxy-config proxy.config.json --port 4201'" > package.json
+
+# Прописываем имя пакета
+cat package.json | json -e "this.name = '@$PROJECTNAME/frontend'" > package.json
 
 rm -rf .init
