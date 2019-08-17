90067e52bb1519cba3c2a04191d8551a2c58b823
Serabass <serabass565@gmail.com>
2019-08-17 06:06:43 +0600

    upd

--- .init/backend/src/app.ts
+++ .init/backend/src/app.ts
@@ -1,17 +1,24 @@
 import express from 'express';
-import { hello } from './modules/hello/hello'
+import {hello} from './modules/hello/hello'
 
 const app = express();
 const port = 3000;
 
 app.get('/', (_, res: any): void => {
-    res.send('Hello World!')
-  },
+        res.send('Hello World!')
+    },
+);
+
+app.get('/api/json', (_, res: any): void => {
+        res.json({
+            value: null,
+        });
+    },
 );
 
 app.listen(port, (): void => {
-    console.log(`Example app listening on port ${port}!`)
-  },
+        console.log(`Example app listening on port ${port}!`)
+    },
 );
 
 console.log(hello('you'));
